7c5bcebacf1196115a76aea0e5ff5af0f1562c47
whoamilq <73354018+whoamilq@users.noreply.github.com>
2024-12-16 22:43:42 +0800

    Add files via upload

---
+++ src/main/java/com/chat_blog/java_agi/api/openai/enums/Role.java
@@ -0,0 +1,17 @@
+package com.chat_blog.java_agi.api.openai.enums;
+
+/**
+ * @Author: huangpenglong
+ * @Date: 2023/3/9 11:09
+ */
+public enum Role {
+    SYSTEM("system"),
+    USER("user"),
+    ASSISTANT("assistant");
+
+    public final String name;
+
+    Role(String name){
+        this.name = name;
+    }
+}
---
+++ src/test/java/com/chat_blog/java_agi/JavaAgiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat_blog.java_agi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAgiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
